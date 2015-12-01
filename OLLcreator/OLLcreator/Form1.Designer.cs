ebabdd139ed951737fab0fb18b1e3529d0135117
Atheros <andrew_987@msn.com>
2015-12-01 11:09:55 -0700

    Basic functionality done, tiff documents not tested.

--- OLLcreator/OLLcreator/Form1.Designer.cs
+++ OLLcreator/OLLcreator/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_Instructions = new System.Windows.Forms.Label();
+            this.label_Source = new System.Windows.Forms.Label();
+            this.textBox_Source = new System.Windows.Forms.TextBox();
+            this.button_Source = new System.Windows.Forms.Button();
+            this.button_Create = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Instructions
+            // 
+            this.label_Instructions.AutoSize = true;
+            this.label_Instructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Instructions.Location = new System.Drawing.Point(12, 9);
+            this.label_Instructions.MaximumSize = new System.Drawing.Size(300, 100);
+            this.label_Instructions.Name = "label_Instructions";
+            this.label_Instructions.Size = new System.Drawing.Size(298, 100);
+            this.label_Instructions.TabIndex = 0;
+            this.label_Instructions.Text = "This tool will create on .oll load file from documents in a folder that uses the " +
+    "page name from the pdf as the Page ID in TrialDirector. The .oll will be placed " +
+    "next to the folder of documents.";
+            // 
+            // label_Source
+            // 
+            this.label_Source.AutoSize = true;
+            this.label_Source.Location = new System.Drawing.Point(13, 143);
+            this.label_Source.Name = "label_Source";
+            this.label_Source.Size = new System.Drawing.Size(218, 13);
+            this.label_Source.TabIndex = 1;
+            this.label_Source.Text = "Choose the folder containing the documents.";
+            // 
+            // textBox_Source
+            // 
+            this.textBox_Source.Location = new System.Drawing.Point(16, 178);
+            this.textBox_Source.Name = "textBox_Source";
+            this.textBox_Source.Size = new System.Drawing.Size(215, 20);
+            this.textBox_Source.TabIndex = 3;
+            // 
+            // button_Source
+            // 
+            this.button_Source.Location = new System.Drawing.Point(237, 176);
+            this.button_Source.Name = "button_Source";
+            this.button_Source.Size = new System.Drawing.Size(75, 23);
+            this.button_Source.TabIndex = 4;
+            this.button_Source.Text = "Browse";
+            this.button_Source.UseVisualStyleBackColor = true;
+            this.button_Source.Click += new System.EventHandler(this.button_Source_Click);
+            // 
+            // button_Create
+            // 
+            this.button_Create.Location = new System.Drawing.Point(123, 231);
+            this.button_Create.Name = "button_Create";
+            this.button_Create.Size = new System.Drawing.Size(75, 23);
+            this.button_Create.TabIndex = 7;
+            this.button_Create.Text = "Create .OLL";
+            this.button_Create.UseVisualStyleBackColor = true;
+            this.button_Create.Click += new System.EventHandler(this.button_Create_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 286);
+            this.Controls.Add(this.button_Create);
+            this.Controls.Add(this.button_Source);
+            this.Controls.Add(this.textBox_Source);
+            this.Controls.Add(this.label_Source);
+            this.Controls.Add(this.label_Instructions);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_Instructions;
+        private System.Windows.Forms.Label label_Source;
+        private System.Windows.Forms.TextBox textBox_Source;
+        private System.Windows.Forms.Button button_Source;
+        private System.Windows.Forms.Button button_Create;
+        private string sourceFolderText = "";
     }
 }
 
--- OLLcreator/OLLcreator/Form1.cs
+++ OLLcreator/OLLcreator/Form1.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using BitMiracle.LibTiff.Classic;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,168 @@ namespace OLLcreator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_Source_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            FolderBrowserDialog FolderBrowserSource = new FolderBrowserDialog();
+
+            DialogResult result = FolderBrowserSource.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                sourceFolderText = FolderBrowserSource.SelectedPath;
+                textBox_Source.Text = sourceFolderText;
+            }
+
+        }
+
+        private void button_Create_Click(object sender, EventArgs e)
+        {
+            create_OLL();
+        }
+
+        private void create_OLL()//TODO
+        {
+            //store all of the files in an array
+            string[] files = Directory.GetFiles(sourceFolderText);
+            MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+            //create oll builders
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbNew = new StringBuilder();
+
+            //look at each file,check for number of pages, and send each page to format_OLL
+            for (int i = 0; i < files.Length; i++)
+            {
+                //check if pdf, tiff, or other
+                FileInfo info = new FileInfo(files[i]);
+                if(info.Extension.Equals(".pdf"))//pdf
+                {
+                    int pages = getNumberOfPdfPages(files[i]);//get number of pages and send to formatter
+                    sbNew = format_OLL(files[i], pages, 5, sb);
+                    sb = sbNew;
+                }
+                else if (info.Extension.Equals(".tiff") || info.Extension.Equals(".tif"))//tiff TODO
+                {
+                    int pages = 1;
+
+                    using (Tiff tiffFromFile = Tiff.Open(files[i], "r"))
+                    {
+                        pages = getNumberofTiffPages(tiffFromFile);
+                    }
+                    sbNew = format_OLL(files[i], pages, 1, sb);
+                    sb = sbNew;
+                }
+                else if (info.Extension.Equals(".mpg"))//media TODO, add other media types as well
+                {
+
+                }
+                else //other (assumes everything that is other is a single page photo, could use some work)
+                {
+                    sb = format_OLL(files[i], 1, 2, sb);
+                    sb = sbNew;
+                }
+                
+                
+            }
+
+            //output oll
+            string folder = "";
+            int index = sourceFolderText.LastIndexOf('\\');
+            folder = sourceFolderText.Substring(0, index);
+
+            string[] paths = new string[] {folder, "LoadFile.oll" };
+            using (StreamWriter outfile = new StreamWriter(Path.Combine(paths)))
+            {
+                outfile.Write(sb.ToString());
+            }
+            MessageBox.Show(".OLL Created");
+        }
+
+        private StringBuilder format_OLL(string sourceFile, int pageNumber, int fileType, StringBuilder sb)//TODO
+        {
+            //Send each page of a document to the formatter, format_OLL_Page.
+            //get a string back that gets appended to the oll
+            //MessageBox.Show(sourceFile + ":" + pageNumber + ":" + fileType);
+            string ollLine = "";
+            for(int i = 1; i <= pageNumber; i++)
+            {
+                if(fileType.Equals(5))//if it is a pdf
+                {
+                    PdfReader pdfReader = new PdfReader(sourceFile);
+                    //string pageName = pdfReader.GetPageN(i).ToString();//this is not correct TODO
+                    //for now it just puts in standard formatting, should have an if that will use this or use the real name
+                    string pageName = Path.GetFileNameWithoutExtension(sourceFile) + "-" + leadingZeros(i);
+                    ollLine = format_OLL_Page(sourceFile, i, fileType, pageName);
+                    sb.AppendLine(ollLine);
+                }
+                //add for tiff and other TODO
+                else if (fileType.Equals(1))
+                {
+                    //for now it just puts in standard formatting, should have an if that will use this or use the real name
+                    string pageName = Path.GetFileNameWithoutExtension(sourceFile) + "-" + leadingZeros(i);
+                    ollLine = format_OLL_Page(sourceFile, i, fileType, pageName);
+                    sb.AppendLine(ollLine);
+                }
+                else
+                {
+                    string pageName = Path.GetFileNameWithoutExtension(sourceFile) + "-" + leadingZeros(i);
+                    ollLine = format_OLL_Page(sourceFile, i, fileType, pageName);
+                    sb.AppendLine(ollLine);
+                }
+            }
+
+            
+            return sb;
+        }
+
+        private string format_OLL_Page(string sourceFile, int pageNumber, int fileType, string pageName)
+        {
+            //Takes the input file and its page number and outputs a string that is one line of the oll.
+            //For multi page files this would be run for each page.
+            //Should check for file type, and if there is a unique page name.
+            string output = "";
+           //MessageBox.Show(sourceFile + ":" + pageNumber + ":" + fileType + ":" + pageName);
+
+
+            string docID = Path.GetFileNameWithoutExtension(sourceFile);
+            string path = Path.GetDirectoryName(sourceFile);
+            string folder = "";
+            int index = path.LastIndexOf('\\');
+            folder = path.Substring(index, path.Length - index);
+            string filename = Path.GetFileName(sourceFile);
+            output = "\"" + fileType + "\",\"" + docID + "\",\"" + pageName + "\",\"" + pageNumber + "\",\"\",\"\",\"" + folder + "\",\"" + filename + "\",\"\"";
+            //MessageBox.Show(output);
+            return output;
+        }
+
+        public int getNumberOfPdfPages(string fileName)
+        {
+            PdfReader pdfReader = new PdfReader(fileName);
+            int numberOfPages = pdfReader.NumberOfPages;
+            return numberOfPages;
+        }
+
+        public int getNumberofTiffPages(Tiff image)
+        {
+            int pageCount = 0;
+            do
+            {
+                ++pageCount;
+            } while (image.ReadDirectory());
+
+            return pageCount;
+        }
+
+        public string leadingZeros(int number)
+        {
+            return number.ToString().PadLeft(3, '0');
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
